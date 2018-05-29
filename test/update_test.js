8eea407c5e43eecbfea58962492c7e8ebf11126d
John Smith <unknown.prop@gmail.com>
2018-05-29 10:33:43 +0900

    Set and Save for Updating Record

--- test/update_test.js
+++ test/update_test.js
@@ -9,5 +9,15 @@ describe("Updating records", () => {
     joe.save().then(() => done());
   });
 
-  it("instance set n save", () => {});
+  it("instance type using set n save", done => {
+    joe.set("name", "Alex");
+    joe
+      .save()
+      .then(() => User.find({}))
+      .then(users => {
+        assert(users.length === 1);
+        assert(users[0].name === "Alex");
+        done();
+      });
+  });
 });
