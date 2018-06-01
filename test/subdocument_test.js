a157c11fca3b2d1e27cd42c296d86a73efb03338
John Smith <unknown.prop@gmail.com>
2018-06-01 12:22:25 +0900

    Adding Subdocuments to Existing Recoreds

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -16,4 +16,25 @@ describe("Subdocuments", () => {
         done();
       });
   });
+
+  it("Can add subdocuments to an existing record", done => {
+    const joe = new User({
+      // 1. create joe
+      name: "Joe",
+      posts: []
+    });
+
+    joe
+      .save() // 2. save joe
+      .then(() => User.findOne({ name: "Joe" })) // 3. fetch joe
+      .then(user => {
+        user.posts.push({ title: "New Post" }); // 4. Add a post to joe
+        return user.save(); // 5. save joe
+      })
+      .then(() => User.findOne({ name: "Joe" })) // 6. fetch joe
+      .then(user => {
+        assert(user.posts[0].title === "New Post"); // 7. make assertion
+        done();
+      });
+  });
 });
